25423c4f88a28e890abad109570fd4428f56ac8c
Bao Haojun <hjbao@marvell.com>
2010-10-26 11:00:46 +0800

    mf

--- mywiki/urls.py
+++ mywiki/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
                        (r'^keyword/(?P<article>.+?)/?$', 'mywiki.gui.views.keyword'),
                        (r'^search/(?P<article>.+?)/?$', 'mywiki.gui.views.search'),
                        (r'^article/(?P<article>.+)$', 'mywiki.gui.views.article'),
-                       (r'^images/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '../mediawiki_sa/images/math'}),
+                       (r'^scripts/images/math/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '../mediawiki_sa/images/math'}),
 
                        # Uncomment this for admin:
                            #(r'^admin/', include('django.contrib.admin.urls')),
