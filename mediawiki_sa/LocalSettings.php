b39eb122d51b44997962f1e87ef417535fe692e6
Bao Haojun <hjbao@marvell.com>
2011-07-04 18:03:42 +0800

    6f9b9dce9f83bd7d04a2741bd0934196e0a444aa for syntax highlight from mediawiki-svn

--- mediawiki_sa/LocalSettings.php
+++ mediawiki_sa/LocalSettings.php
@@ -28,6 +28,7 @@ $path = array( $IP, "$IP/includes", "$IP/languages" );
 set_include_path( implode( PATH_SEPARATOR, $path ) . PATH_SEPARATOR . get_include_path() );
 
 require_once( "$IP/includes/DefaultSettings.php" );
+require_once("$IP/extensions/SyntaxHighlight_GeSHi/SyntaxHighlight_GeSHi.php");
 
 # If PHP's memory limit is very low, some operations may fail.
 # ini_set( 'memory_limit', '20M' );
