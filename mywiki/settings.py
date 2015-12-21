455a530e31ca7bc821688de18877c44abad624b5
Bao Haojun <baohaojun@gmail.com>
2015-12-21 12:04:53 +0800

    Fix server error, from stackexchange.
    Signed-off-by: Bao Haojun <baohaojun@gmail.com>

--- mywiki/settings.py
+++ mywiki/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.doc.XViewMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',
 )
 
 ROOT_URLCONF = 'mywiki.urls'
